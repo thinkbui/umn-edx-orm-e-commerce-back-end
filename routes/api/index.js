4ae6d45ad56c45954b58dd79dcf19616080951ab
Ian Bui <thinkbui@gmail.com>
2023-05-10 23:22:28 -0500

    Added tag routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router()
 const categoryRoutes = require("./categoryRoutes");
 const productRoutes = require("./productRoutes");
+const tagRoutes = require("./tagRoutes");
 
 
 router.use("/category", categoryRoutes);
 router.use("/product", productRoutes);
+router.use("/tag", tagRoutes);
 
 
 module.exports = router;
